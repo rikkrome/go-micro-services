9b21cde5699213d5e0e0799009b59fa28340cac5
ricky romero <contact@romeroricky.com>
2023-09-30 14:20:13 -0700

    CreateUserHandler updates

--- services/user-service/api/controllers/user-controller.go
+++ services/user-service/api/controllers/user-controller.go
@@ -5,14 +5,40 @@ import (
 	"net/http"
 )
 
+// Model
+type User struct {
+	Name     string
+	Username string
+}
+
+type (
+	// For Post/Put - /users
+	UserResource struct {
+		Data User `json:"data"`
+	}
+)
+
+func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var dataResource UserResource
+
+	// Decode the incoming User json
+	err := json.NewDecoder(r.Body).Decode(&dataResource)
+	user := &dataResource.Data
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Send response back
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
+}
+
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	// A very simple health check.
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-
-	// In the future we could report back on the status of our DB, or our cache
-	// (e.g. Redis) by performing a simple PING, and include them in the response.
-	// io.WriteString(w, `{"alive": true}`)
 	json.NewEncoder(w).Encode(map[string]bool{"alive": true})
 
 }
