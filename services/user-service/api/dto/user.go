23417c4891266ba2669cbbadc7780d79e61b3cfe
ricky romero <contact@romeroricky.com>
2023-10-09 18:23:33 -0700

    user service: Validate create user

--- services/user-service/api/dto/user.go
+++ services/user-service/api/dto/user.go
@@ -3,10 +3,10 @@ package dto
 type (
 	// create new user
 	CreateUser struct {
-		FirstName string `json:"firstname" validate:"required,min=2"`
-		LastName  string `json:"lastname" validate:"required,min=2"`
-		Username  string `json:"username" validate:"required,min=2"`
-		Email     string `json:"email" validate:"required,min=2,email"`
+		FirstName string `json:"firstname" validate:"required"`
+		LastName  string `json:"lastname" validate:"required"`
+		Username  string `json:"username" validate:"required,min=3"`
+		Email     string `json:"email" validate:"required,email"`
 		Password  string `json:"password" validate:"required,min=8"`
 	}
 
--- services/user-service/api/models/user-model.go
+++ services/user-service/api/models/user-model.go
@@ -1,12 +1,8 @@
 package models
 
 import (
-	"errors"
-	"fmt"
 	"log"
-	"time"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"github.com/rikkrome/go-micro-services/services/user-service/api/dto"
 	"golang.org/x/crypto/bcrypt"
@@ -21,7 +17,6 @@ type (
 		Username       string    `gorm:"column:username;unique"`
 		Email          string    `gorm:"uniqueIndex,column:email;unique"`
 		HashedPassword string    `gorm:"column:hashed_password"`
-		CreatedAt      time.Time
 	}
 
 	UserModel struct {
@@ -35,17 +30,9 @@ func NewUserModel(db *gorm.DB) *UserModel {
 }
 
 func (m *UserModel) Create(user *dto.CreateUser) error {
-	// Validate the DTO
-	validate := validator.New()
-	err := validate.Struct(user)
-	errorMessage := ""
+	err := dto.Validate(user)
 	if err != nil {
-		for _, err := range err.(validator.ValidationErrors) {
-			field := err.Field()
-			tag := err.Tag()
-			errorMessage += fmt.Sprintf("%s is %s\n", field, tag)
-		}
-		return errors.New(errorMessage)
+		return err
 	}
 	newUUID, err := uuid.NewRandom() // Generate a new UUID
 	if err != nil {
@@ -53,8 +40,6 @@ func (m *UserModel) Create(user *dto.CreateUser) error {
 	}
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
-		// handle error
-		log.Printf("Error GenerateFromPassword: %v", err)
 		return err
 	}
 	newUser := &User{
@@ -62,6 +47,7 @@ func (m *UserModel) Create(user *dto.CreateUser) error {
 		FirstName:      user.FirstName,
 		LastName:       user.LastName,
 		Username:       user.Username,
+		Email:          user.Email,
 		HashedPassword: string(hashedPassword),
 	}
 	result := m.DB.Create(newUser)
